d7c6041cb087fad2cbe29c5d8e12cfdd3fc52029
Erik Norvelle <erik.m.norvelle@gmail.com>
2014-04-29 19:14:48 -0700

    Fixes to implement initial individual unit tests

---
+++ AddressDiscoverer/test/org/norvelle/addressdiscoverer/model/IsNameTest.java
@@ -0,0 +1,56 @@
+/**
+ * Part of the AddressDiscoverer project, licensed under the GPL v.3 license.
+ * This project provides intelligence for discovering email addresses in
+ * specified web pages, associating them with a given institution and department
+ * and address type.
+ *
+ * This project is licensed under the GPL v.3. Your rights to copy and modify
+ * are regulated by the conditions specified in that license, available at
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ */
+package org.norvelle.addressdiscoverer.model;
+
+import com.j256.ormlite.support.ConnectionSource;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.norvelle.addressdiscoverer.TestUtilities;
+import org.norvelle.addressdiscoverer.exceptions.CannotLoadJDBCDriverException;
+import org.norvelle.addressdiscoverer.exceptions.CantParseIndividualException;
+
+/**
+ *
+ * @author Erik Norvelle <erik.norvelle@cyberlogos.co>
+ */
+public class IsNameTest {
+    
+    // A logger instance
+    private static final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME); 
+    private static ConnectionSource connection;
+
+    public IsNameTest() {
+    }
+    
+    @BeforeClass
+    @SuppressWarnings("UnnecessaryReturnStatement")
+    public static void setUpClass() {
+        TestUtilities.setupLogger();
+        try {
+            connection = TestUtilities.getDBConnection("addresses.sqlite");
+        } catch (SQLException | CannotLoadJDBCDriverException | IOException ex) {
+            fail("Encountered problems connecting to database: " + ex.getMessage());
+            return;
+        }
+    }
+
+    @Test
+    public void testArnuncio() {
+        boolean isName = Name.isName("Arnuncio Pastor, J.C.");
+        Assert.assertTrue("isName should be true for Arnuncio Pastor, J.C.", isName);
+    }
+    
+}
